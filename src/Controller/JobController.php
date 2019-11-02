7855ab35b633337b1797a9b2242c5df6e6c2d127
fclausset <frederic.clausset@gmail.com>
2019-11-02 18:03:42 +0100

    WIP : file upload in the controller

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -8,7 +8,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Job;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use App\Form\JobType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 
 class JobController extends AbstractController
 {
@@ -43,6 +48,50 @@ class JobController extends AbstractController
     {
         return $this->render('job/show.html.twig', [
             'job' => $job,
+        ]);
+    }
+
+
+
+    /**
+     * Creates a new job entity.
+     *
+     * @Route("/job/create", name="job.create", methods={"GET", "POST"})
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     *
+     * @return RedirectResponse|Response
+     */
+    public function create(Request $request, EntityManagerInterface $em) : Response
+    {
+        $job = new Job();
+        $form = $this->createForm(JobType::class, $job);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile|null $logoFile */
+            $logoFile = $form->get('logo')->getData();
+
+            if ($logoFile instanceof UploadedFile) {
+                $fileName = \bin2hex(\random_bytes(10)) . '.' . $logoFile->guessExtension();
+
+                // moves the file to the directory where brochures are stored
+                $logoFile->move(
+                    $this->getParameter('jobs_directory'),
+                    $fileName
+                );
+
+                $job->setLogo($fileName);
+            }
+            $em->persist($job);
+            $em->flush();
+
+            return $this->redirectToRoute('job.list');
+        }
+
+        return $this->render('job/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
